654af413da233ba45d1c5c7048c3e3fdd4b3bf64
karlos_mafra <kadumaframoraes@gmail.com>
2024-12-07 09:07:48 -0300

    estilo simulador

--- js/crimes.js
+++ js/crimes.js
@@ -4,6 +4,10 @@ const dialogCrimes = document.getElementById("dialog-crimes")
 const btnCloseDialogCrimes = document.getElementById("btn-close-dialog-crimes")
 const btnSubmitCrime = document.getElementById("btn-submit-crime")
 
+const listCrimes = document.getElementById("list-crimes")
+let crimes = JSON.parse(localStorage.getItem("crimes")) || [];
+let registerLocalStorage = getRegisterLocalStorage()
+
 const inCrimeName = document.getElementById("in-crime-name")
 const inCrimeDesc = document.getElementById("in-crime-desc")
 const inCrimeDate = document.getElementById("in-crime-date")
@@ -30,6 +34,10 @@ async function submitCrime() {
         "level": inCrimeLevel.value,
     }
 
+    saveRegisterLocalStorage(crime)
+
+    dialogCrimes.close()
+
     /*
     fetch('http://localhost:3000/crimes', {
         method: 'POST',
@@ -46,6 +54,51 @@ async function submitCrime() {
 
 function openDialogCrimes() {
 
+    inCrimeName.value = ""
+    inCrimeDesc.value = ""
+    inCrimeDate.value = ""
+    inCrimeHero.value = ""
+    inCrimeLevel.value = 1
+    crimeLevelValue.textContent = 1
+
     dialogCrimes.showModal()
 
 }
+
+function getCrimesLocal() {
+
+    listCrimes.innerHTML = ""
+
+    crimes.forEach((crime, index) => {
+        const linha = document.createElement("tr")
+        linha.innerHTML = `
+            <td>${crime.name}</td>
+            <td>${crime.desc}</td>
+            <td>${crime.date}</td>
+            <td>${crime.hero}</td>
+            <td>${crime.level}</td>
+            `
+        listCrimes.appendChild(linha)
+    })
+
+}
+
+function saveRegisterLocalStorage(register) {
+
+    registerLocalStorage.push(register)
+    localStorage.setItem("crimes", JSON.stringify(registerLocalStorage))
+}
+  
+function getRegisterLocalStorage(register) {
+      
+    let registers = JSON.parse(localStorage.getItem(register))
+  
+    if (!registers) {
+        return []
+    }
+  
+    return registers
+  
+}
+
+getCrimesLocal()
